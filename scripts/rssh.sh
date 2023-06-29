e81f79af0cf6e19254045f3f60b22d7cbe59540a
Gauthier FRANÇOIS <gauthier.francois@doctolib.com>
2023-06-29 09:12:40 +0200

    fix: lot of screen in the same time

--- scripts/rssh.sh
+++ scripts/rssh.sh
@@ -2,4 +2,13 @@
 
 host_name=`hostname`
 nu=${#host_name}
-/usr/bin/screen  -dmS rSSH bash -c "ssh -R 598$nu:localhost:22 rescue"
+
+already=`ps faux | grep "[ssh] -R" | grep -v SCREEN | wc -l`
+
+if [ $already -gt 1 ]
+then
+	echo "${date} - already monted"
+else
+	/usr/bin/screen  -dmS rSSH bash -c "ssh -R 598$nu:localhost:22 rescue"
+	sleep 10
+fi
