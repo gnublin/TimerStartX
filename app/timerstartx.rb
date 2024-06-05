2aa6cc3e9d029b1e89dcded368ecf6ac31383f13
Gauthier FRANÇOIS <gauthier.francois@resilience.care>
2024-06-05 11:34:52 +0200

    feat: Add archives and refact results

--- app/timerstartx.rb
+++ app/timerstartx.rb
@@ -65,6 +65,7 @@ class TimerStartX < Sinatra::Application
     @run_in_progress = @runs.select { |_, status| status == 'started' }.keys.first
     @runs_in = {}
     @all_competitors = hm_getall(redis, 'competitors')
+    @all_archives = Dir.children("archives") || []
     @portals_status = redis.hgetall('portals')
     unless @run_in_progress.nil?
       competitors_inprogress.each_with_index do |competitor, idx|
@@ -113,28 +114,6 @@ class TimerStartX < Sinatra::Application
     slim :view
   end
 
-  get '/rank_by_avg' do
-    @competitors_avg = {}
-    @all_competitors.each do |nb, infos|
-      avg_time = infos.map { |_, t| t['ts_time'] }.reject!(&:nil?)
-      avg_time -= ['crashed']
-      @competitors_avg[nb] = avg_time.sum(0.0) / avg_time.size unless avg_time.empty?
-    end
-    @competitors_avg = @competitors_avg.sort_by { |_, v| v }.to_h
-    slim :rank_by_avg
-  end
-
-  get '/rank_by_min' do
-    @competitors_min = {}
-    @all_competitors.each do |nb, infos|
-      min_time = infos.map { |_, t| t['ts_time'] }.reject!(&:nil?)
-      min_time -= ['crashed']
-      @competitors_min[nb] = min_time.min unless min_time.empty?
-    end
-    @competitors_min = @competitors_min.sort_by { |_, v| v}.to_h
-    slim :rank_by_min
-  end
-
   get '/vote' do
     @vote_msg = 'Vous avez déjà voté. Merci !' if request.cookies['L4D3sc3nt3DuM3nh1rV0t3'] == 'true'
     @vote_msg = "Il n'est pas possible de voter" if @vote_state == 'close'
@@ -167,6 +146,37 @@ class TimerStartX < Sinatra::Application
     redirect '/vote'
   end
 
+  get ["/results", "/archives"] do
+    if request.path_info == "/archives"
+      @results_title = "Archives #{params['year']}"
+      base_path = "archives/#{params['year']}"
+      all_files = Dir["#{base_path}/*"]
+      if all_files.empty?
+        @year_all_competitors = {}
+      else
+        file_competitors = all_files.select{|filename| filename.match? /competitors/ }.first
+        file_runs = all_files.select{|filename| filename.match? /runs/ }.first
+        @year_all_competitors = JSON.parse(File.read(file_competitors))
+        year_runs = JSON.parse(File.read(file_runs))
+      end
+    else
+      @results_title = 'Résultats'
+      @year_all_competitors = @all_competitors
+    end
+    @year_competitors_min = {}
+    @year_competitors_avg = {}
+    @year_all_competitors.each do |nb, infos|
+      t_time = infos.map { |_, t| t['ts_time'] }.reject!(&:nil?)
+      t_time -= ['crashed']
+      @year_competitors_min[nb] = t_time.min unless t_time.empty?
+      @year_competitors_avg[nb] = t_time.sum(0.0) / t_time.size unless t_time.empty?
+    end
+
+    @year_competitors_avg = @year_competitors_avg.sort_by { |_, v| v }.to_h
+    @year_competitors_min = @year_competitors_min.sort_by { |_, v| v}.to_h
+    slim :results
+  end
+
   get '/disconnect' do
     redirect '/'
   end
@@ -194,6 +204,22 @@ class TimerStartX < Sinatra::Application
       redis.set('vote', params['vote_action'])
       redis.close
       redirect '/admin/vote'
+    end
+
+    get '/archives' do
+      slim :admin_archives
+    end
+
+    post '/download_competitors' do
+      response.headers['content_type'] = "application/octet-stream"
+      attachment("#{Time.now.year}-all_competitors.json")
+      response.write(@all_competitors.to_json)
+    end
+
+    post '/download_runs' do
+      response.headers['content_type'] = "application/octet-stream"
+      attachment("#{Time.now.year}-all_runs.json")
+      response.write(@runs.to_json)
     end
 
     post '/modify_run' do
